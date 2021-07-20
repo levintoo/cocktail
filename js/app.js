6df6e82db075b8b9b9306f7bbe087ab5be9acf4f
levintoo <tooklevin@gmail.com>
2021-07-20 17:35:59 +0300

    found food message

--- js/app.js
+++ js/app.js
@@ -9,75 +9,47 @@ function getMealList() {
   let searchInput = document.querySelector('#search-input').value.trim();
   console.log(searchInput);
   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
-  .then(response => response.json())
-  .then(data =>{
+    .then(response => response.json())
+    .then(data => {
 
-    if(data.meals){
+      if (data.meals) {
         data.meals.forEach(meal => {
+          document.querySelector('.feedback-title').innerHTML=("Food found");
+          document.querySelector('.feedback-title').classList.add("text-success");
 
-const recipeCard = document.createElement('div');
-recipeCard.classList.add(`col-lg-3`,`col-md-5`,`col-sm-10`,`recipe-card`)
-mealContainer.appendChild(recipeCard);
-recipeCard.setAttribute("id", `${meal.idMeal}`);
+          const recipeCard = document.createElement('div');
+          recipeCard.classList.add(`col-lg-3`, `col-md-5`, `col-sm-10`, `recipe-card`)
+          mealContainer.appendChild(recipeCard);
+          recipeCard.setAttribute("id", `${meal.idMeal}`);
 
-const recipeCardImage = document.createElement('img');
-recipeCard.appendChild(recipeCardImage);
-recipeCardImage.setAttribute("src", `${meal.strMealThumb}`);
-
-
-const cardDescriptionDiv = document.createElement('div');
-cardDescriptionDiv.classList.add(`card-description`);
-recipeCard.appendChild(cardDescriptionDiv);
-
-const carddecriptionname= document.createElement('h4');
-carddecriptionname.classList.add(`food-n`);
-cardDescriptionDiv.appendChild(carddecriptionname);
-carddecriptionname.innerHTML=(`${meal.strMeal}`)
-
-const carddecriptionButton= document.createElement('button');
-carddecriptionButton.setAttribute("id", "get-recipe-btn");
-carddecriptionButton.setAttribute("data-bs-toggle", "modal");
-carddecriptionButton.setAttribute("data-bs-target", "#staticBackdrop");
-cardDescriptionDiv.appendChild(carddecriptionButton);
-carddecriptionButton.innerHTML=("Get Recipe")
+          const recipeCardImage = document.createElement('img');
+          recipeCard.appendChild(recipeCardImage);
+          recipeCardImage.setAttribute("src", `${meal.strMealThumb}`);
 
 
+          const cardDescriptionDiv = document.createElement('div');
+          cardDescriptionDiv.classList.add(`card-description`);
+          recipeCard.appendChild(cardDescriptionDiv);
 
- 
+          const carddecriptionname = document.createElement('h4');
+          carddecriptionname.classList.add(`food-n`);
+          cardDescriptionDiv.appendChild(carddecriptionname);
+          carddecriptionname.innerHTML = (`${meal.strMeal}`)
+
+          const carddecriptionButton = document.createElement('button');
+          carddecriptionButton.setAttribute("id", "get-recipe-btn");
+          carddecriptionButton.setAttribute("data-bs-toggle", "modal");
+          carddecriptionButton.setAttribute("data-bs-target", "#staticBackdrop");
+          cardDescriptionDiv.appendChild(carddecriptionButton);
+          carddecriptionButton.innerHTML = ("Get Recipe")
+
+
+
+
         });
-    }
-  })
+      }else{
+        document.querySelector('.feedback-title').innerHTML=("Food could not be found");
+        document.querySelector('.feedback-title').classList.add("text-danger");
+      }
+    })
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // async function getdata(){
-  //   let response= await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
-  //   let data= await response.json();
-   
- 
-  //       console.log(data);
-
-    
-  // }
-
-  // getdata();
- 
